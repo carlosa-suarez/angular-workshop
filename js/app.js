dda02d405a55bb8f12e45430319a80adf5e01629
carlosa-suarez <ingcarlos.suarez.moreno@gmail.com>
2015-12-09 17:40:26 -0500

    local images and print data

--- js/app.js
+++ js/app.js
@@ -7,23 +7,21 @@
 	function AppMainCtrl() {
 		this.products = productObj;
 	}
-
+	/*
 	app.controller('PanelCtrl', PanelCtrl(detail));
 	function PanelCtrl(detail){
-		this.description = detail.description;
-
-		
+		this.description = detail.description;	
 	};
-
+	*/
 
 	var productObj = [
 		{
 			"id":"1",
 			"name": "family guy",
 			"image": [
-				"http://static.independent.co.uk/s3fs-public/thumbnails/image/2014/03/12/12/family%20guy.jpg",
-				"http://s2.favim.com/orig/28/family-guy-funny-griffin-haha-lmao-Favim.com-233176.gif",
-				"http://www.zemwallpapers.com/wp-content/uploads/2015/08/Family-Guy-Funny-Pictures-5.jpg"
+				"img/family1.jpg",
+				"img/family2.gif",
+				"img/family3.jpg"
 			],
 			"details":{
 				"description":"Lorem ipsum 1",
@@ -53,9 +51,9 @@
 			"id":"2",
 			"name": "simpson",
 			"image": [
-				"http://cdndata.bigfooty.com/2015/02/106075_85609c587aedee9e54ef77a43b1f11f4.jpg",
-				"http://xaxor.com/wp-content/uploads/2012/12/Funny-witty-Simpsons-moments3.jpg",
-				"http://i.imgur.com/br5EWbc.png"
+				"img/simpson1.jpg",
+				"img/simpson2.jpg",
+				"img/simpson3.jpg"
 			],
 			"details":{
 				"description":"Lorem ipsum 2",
@@ -84,9 +82,9 @@
 			"id":"3",
 			"name": "Humor Colombiano",
 			"image": [
-				"https://i.ytimg.com/vi/9vU_l4Ttts0/maxresdefault.jpg",
-				"https://i.ytimg.com/vi/p7NUMsSznWI/hqdefault.jpg",
-				"http://m.vive.in/contenido/planes/bogota/IMAGEN/IMAGEN-9744064-2.JPG"
+				"img/humor1.jpg",
+				"img/humor2.jpg",
+				"img/humor3.jpg"
 			],
 			"details":{
 				"description":"Lorem ipsum 3",
